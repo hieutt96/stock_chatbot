57b65e9eaf50c68018e0ad614653b1da877d1a8b
Trung Hieu Tran <hieutt.bka59@gmail.com>
2018-05-11 12:55:23 +0700

    first commit

---
+++ train.py
@@ -0,0 +1,81 @@
+import tflearn
+import tensorflow as tf
+import numpy as np
+import json
+from pyvi import ViTokenizer, ViPosTagger
+import pickle
+import nltk
+import random
+
+with open('data/intents.json',encoding="utf-8") as json_data:
+	intents = json.load(json_data)
+
+words = []
+words1= []
+documents = []
+classes = []
+stop_words = ['bị', 'bởi', 'cả', 'các', 'cái', 'cần', 'càng', 'chỉ', 'chiếc', 'cho', 'chứ', 'chưa', 'chuyện', 'có',
+              'có_thể', 'cứ', 'của', 'cùng', 'cũng', 'đã', 'đang', 'đây', 'để', 'đến nỗi', 'đều', 'điều', 'do', 'đó',
+              'được', 'dưới', 'gì', 'khi', 'không', 'là', 'lại', 'lên', 'lúc', 'mà', 'mỗi', 'một_cách', 'này', 'nên',
+              'nếu', 'ngay', 'nhiều', 'như', 'nhưng', 'những', 'nơi', 'nữa', 'phải', 'qua', 'ra', 'rằng', 'rất',
+              'theo', 'thì', 'sẽ', 'rồi', 'sau', 'tại', 'trên', 'trước', 'từ', 'từng', 'và', 'vẫn', 'vào', 'vậy',
+              'vừa', 'với', 'vì', '?', 'à', 'ừ', 'ạ']
+
+for intent in intents['intents']:
+    for pattern in intent['patterns']:
+        w = ViPosTagger.postagging(ViTokenizer.tokenize(pattern))[0]
+        # print(w)
+        words.extend(w)
+        documents.append((w, intent['tag']))
+    if intent['tag'] not in classes:
+        classes.append(intent['tag'])
+
+for w in words:
+    if w not in stop_words:
+        words1.append(w)
+words = sorted(list(set(words1)))
+
+# Create training data
+
+training = []
+output = []
+output_empty = [0] * len(classes)
+
+for doc in documents:
+    bag = []
+    pattern_words = doc[0]
+
+    for w in words:
+        if w in pattern_words:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    # output is a '0' for each tag and '1' for current tag
+    output_row = list(output_empty)
+    output_row[classes.index(doc[1])] = 1
+
+    training.append([bag, output_row])
+
+# shuffle our features and turn into np.array
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net, optimizer='adam', loss='categorical_crossentropy')
+
+model =  tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
+model.save('models/model.tflearn')
+
+pickle.dump({'words': words, 'classes': classes, 'train_x': train_x, 'train_y': train_y},
+            open("models/training_data", "wb"))
